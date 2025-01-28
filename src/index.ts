0cf978e0179475cedd2c41ae6afeb56d1331af2f
nelson chibukasia <chibukasianelson@gmail.com>
2025-01-28 23:53:38 +0300

    added auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser = require("body-parser");
+import cors from 'cors'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Route restricted to /api");
 });
 
 app.listen(port, () => {
---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,32 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+
+type User ={
+    id: string,
+    role: string,
+}
+declare module 'express' {
+    interface Request {
+      user?: User 
+    }
+  }
+  
+
+const verifyToken = async (req: Request, res: Response, next: NextFunction) =>{
+    const token = req.headers.authorization?.split(' ')[1]
+    if(!token){
+     return res.status(401).json({ error: 'No token provided, not authenticated' })
+    }
+    jwt.verify(token, 'hash', (error, decoded)=> {
+        if(error){
+            console.log(error)
+            return res.status(401).json({error: 'Invalid token, not authenticated'})
+        }
+        req.user = decoded as unknown as User
+        next()
+    
+    } )
+    
+}
+
+export default verifyToken
