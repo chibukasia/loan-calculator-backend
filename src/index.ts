d88f413932ab2a0eeec17873fbc1ab3325de5c3f
nelson chibukasia <chibukasianelson@gmail.com>
2025-01-31 01:47:16 +0300

    changed port

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import loans from './routes/loan'
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
